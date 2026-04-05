12a89b3b8cf271db0643640fd09b7ddfe430808d
aannoni <annonialexandra@gmail.com>
2026-04-05 19:19:53 +1000

    ready to start

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import './assets/main.css'
+import 'element-plus/dist/index.css' // 1. Add Element Plus Styles
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus' // 2. Add Element Plus Library
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus) // 3. Tell Vue to use Element Plus
 
 app.mount('#app')
